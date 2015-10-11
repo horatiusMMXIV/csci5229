55ea4b10c2187479842a8d23afec5cd5c011ffe3
Bob <Bob@192.168.1.8>
2015-10-11 16:13:59 -0600

    Initial commit of hw6.

---
+++ hw6/key.c
@@ -0,0 +1,75 @@
+#include "CSCIx229.h"
+
+
+/*
+ *  GLUT calls this routine when a key is pressed
+ */
+void key(unsigned char ch,int x,int y)
+{
+   //  Exit on ESC
+   if (ch == 27)
+      exit(0);
+   //  Reset view angle
+   else if (ch == '0')
+      th = ph = 0;
+   //  Toggle axes
+   else if (ch == 'x' || ch == 'X')
+      axes = 1-axes;
+   //  Toggle lighting
+   else if (ch == 'l' || ch == 'L')
+      light = 1-light;
+   //  Switch projection mode
+   else if (ch == 'p' || ch == 'P')
+      mode = 1-mode;
+   //  Toggle light movement
+   else if (ch == 'm' || ch == 'M')
+      move = 1-move;
+   //  Move light
+   else if (ch == '<')
+      zh += 1;
+   else if (ch == '>')
+      zh -= 1;
+   //  Change field of view angle
+   else if (ch == '-' && ch>1)
+      fov--;
+   else if (ch == '+' && ch<179)
+      fov++;
+   //  Light elevation
+   else if (ch=='[')
+      ylight -= 0.1;
+   else if (ch==']')
+      ylight += 0.1;
+   //  Ambient level
+   else if (ch=='a' && ambient>0)
+      ambient -= 5;
+   else if (ch=='A' && ambient<100)
+      ambient += 5;
+   //  Diffuse level
+   else if (ch=='d' && diffuse>0)
+      diffuse -= 5;
+   else if (ch=='D' && diffuse<100)
+      diffuse += 5;
+   //  Specular level
+   else if (ch=='s' && specular>0)
+      specular -= 5;
+   else if (ch=='S' && specular<100)
+      specular += 5;
+   //  Emission level
+   else if (ch=='e' && emission>0)
+      emission -= 5;
+   else if (ch=='E' && emission<100)
+      emission += 5;
+   //  Shininess level
+   else if (ch=='n' && shininess > 0)
+      shininess -= 1;
+   else if (ch=='N' && shininess<7)
+      shininess += 1;
+   //  Translate shininess power to value (-1 => 0)
+   shinyvec[0] = shininess <= 0 ? 0 : pow(2.0,shininess);
+   //  Reproject
+   Project(fov, asp, dim);
+   //  Animate if requested
+   glutIdleFunc(move?idle:NULL);
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
